f93825093bcb1bc4b48a87ec5ffe0c36e32b5771
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-07-03 06:29:07 +0800

    Fixing bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+//styles
+import "./App.css";
+
+//components
+import Dashboard from "./page/dashboard/Dashboard";
+import Create from "./page/create/Create";
+import Login from "./page/login/Login";
+import Signup from "./page/signup/Signup";
+import Project from "./page/project/Project";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,20 +5,21 @@ import "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyDEJimhAyi7mD5wIUWu0NbosoBukY7NyEs",
   authDomain: "thedojosite-e39cd.firebaseapp.com",
+  databaseURL: "https://thedojosite-e39cd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "thedojosite-e39cd",
   storageBucket: "thedojosite-e39cd.appspot.com",
   messagingSenderId: "726558318440",
-  appId: "1:726558318440:web:b2fc49c33a70b6454380dd",
+  appId: "1:726558318440:web:b2fc49c33a70b6454380dd"
 };
 
-//init firebase
+// init firebase
 firebase.initializeApp(firebaseConfig);
 
-//init services
+// init service
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
 
-//timestamp
+// timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { projectFirestore, projectAuth, timestamp}
+export { projectFirestore, projectAuth, timestamp };
